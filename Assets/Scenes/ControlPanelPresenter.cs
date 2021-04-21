bf12c55f979cb79b9856c1ed2f812c75e0dfdd91
Pavel Dubovik <pavel.dubovik@gismart.com>
2021-04-22 01:58:27 +0300

    implement Presenter and NetworkClient

---
+++ Assets/Scenes/ControlPanelPresenter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scenes
+{
+    public class ControlPanelPresenter : MonoBehaviour
+    {
+        [SerializeField] private GameObject connectingToServerWidget;
+        [SerializeField] private Button turnLightOnButton;
+        [SerializeField] private Button turnLightOffButton;
+        [SerializeField] private Button makeExplosionButton;
+
+        private NetworkClient networkClient;
+
+        private void Start()
+        {
+            networkClient = new NetworkClient();
+            
+            turnLightOnButton.OnClickAsObservable().Subscribe(_ => networkClient.SendMessage(NetworkMessage.TurnLightOn));
+            turnLightOffButton.OnClickAsObservable().Subscribe(_ => networkClient.SendMessage(NetworkMessage.TurnLightOff));
+            makeExplosionButton.OnClickAsObservable().Subscribe(_ => networkClient.SendMessage(NetworkMessage.MakeExplosion));
+            
+            networkClient.IsConnectedToServer.Subscribe(SetupView);
+            
+            SetupView(false);
+            networkClient.ConnectToServerAsync();
+        }
+
+        private void SetupView(bool isConnectedToServer)
+        {
+            connectingToServerWidget.gameObject.SetActive(!isConnectedToServer);
+            turnLightOnButton.interactable = turnLightOffButton.interactable =
+                makeExplosionButton.interactable = isConnectedToServer;
+        }
+
+        private void OnDestroy()
+        {
+            networkClient.Dispose();
+        }
+    }
+}
---
+++ Assets/Scenes/NetworkClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using UniRx;
+using UnityEngine;
+
+namespace Scenes
+{
+    public class NetworkClient : IDisposable
+    {
+        private const int Port = 8888;
+        private const string IP = "127.0.0.1";
+        
+        private TcpClient client;
+        private NetworkStream dataStream;
+        
+        public BoolReactiveProperty IsConnectedToServer { get; private set; }
+
+        public NetworkClient()
+        {
+            IsConnectedToServer = new BoolReactiveProperty(false);
+        }
+
+        public async void ConnectToServerAsync()
+        {
+            client = new TcpClient();
+            await client.ConnectAsync(IP, Port);
+            IsConnectedToServer.Value = true;
+            dataStream = client.GetStream();
+        }
+
+        public void SendMessage(NetworkMessage message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(JsonUtility.ToJson(message)); // использовать .NET JSON?
+            dataStream.Write(data, 0, data.Length);
+        }
+
+        public void Dispose()
+        {
+            client?.Dispose();
+            dataStream?.Dispose();
+            IsConnectedToServer?.Dispose();
+        }
+    }
+}
